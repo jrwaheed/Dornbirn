18e26cc0625a6c1899ad8a443057847a2e9fddcb
jrwaheed <j.r.waheed@gmail.com>
2025-10-01 19:41:09 +0200

    Conditional rendering

--- React/frontend/src/App.jsx
+++ React/frontend/src/App.jsx
@@ -1,19 +1,31 @@
 
 import './App.css'
+import MovieCard from './components/MovieCard'
 
 function App() {
+  const movieNumber = 1
+  /* HERE IS THE KICKER. WE ARE USING THE MOVIECARD FUNCTION AND PASSING
+  AN OBJECT AS AN ARGUMENT. THIS ARGUMENT SERVES AS THE PROPERTY THE BEING
+  PASSED. THE OUTER CURLY BRACKETS SIGNIFY THE A VARIABLE, THE SECOND SET
+  ARE THE OBJECT.*/
+
   return (
     <>
-      <Monkey value="Hello, World!" />
-      <Monkey value="This is a monkey component." />
+
+      {/* THIS IS CONDITIONAL RENDERING  */}
+      {movieNumber === 1 ? (
+        <MovieCard movie={{ title: "jamals film", release_date: "1999" }} />
+      ) : (
+        <MovieCard movie={{ title: "Ursulas blovkbuster", release_date: "2004" }} />
+      )}
     </>
-  )
+  );
 }
+
 
 function Monkey({ value }) {
   return (
     <div>
-      <p>{value}</p>
     </div>
   )
 }
